7ed8d06dedd348aa9e1cbe87042f571714f0b9fb
Francesco Amato <francamato@gmail.com>
2021-04-14 10:40:09 +0200

    fix to server

--- cosmo-backend/R-server/api-controller.R
+++ cosmo-backend/R-server/api-controller.R
@@ -16,11 +16,11 @@ library(tidyverse)
 
 #setwd("/home/is2admin/hackathon/git/hack-backend/R-server")
 #setwd("C:\\Users\\ibuku\\git\\hack-backend\\R-server")
-# basedir = ("/home/is2admin/hackathon/git/hack-backend/R-server/rscript")
-# basedirData=("/home/is2admin/hackathon/git/hack-backend/R-server/data")
+basedir = ("/home/is2admin/hackathon/git/cosmopolitics/cosmo-backend/R-server/rscript")
+basedirData=("/home/is2admin/hackathon/git/cosmopolitics/cosmo-backend/R-server/data")
 
-basedir = ("C:\\Users\\federico\\git\\cosmopolitics\\cosmo-backend\\R-server\\rscript")
-basedirData=("C:\\Users\\federico\\git\\cosmopolitics\\cosmo-backend\\R-server\\data")
+#basedir = ("C:\\Users\\federico\\git\\cosmopolitics\\cosmo-backend\\R-server\\rscript")
+#basedirData=("C:\\Users\\federico\\git\\cosmopolitics\\cosmo-backend\\R-server\\data")
 
 #basedir = ("d:/development/cosmopolitics/cosmo-backend/R-server/rscript")
 #basedirData=("d:/development/cosmopolitics/cosmo-backend/R-server/data")
@@ -68,12 +68,12 @@ COMEXT_EXP<-load_comext("2")
 #PlotCompRes<-PlotMobComp("Italy","Italy")
 #Indicator  <-PolInd("Italy","Italy")
 
-flow<-2
-country_code<-"IT"
-partner_code<-"US"
-var_bec<-7
-year<-2020
-month<-2
+#flow<-2
+#country_code<-"IT"
+#partner_code<-"US"
+#var_bec<-7
+#year<-2020
+#month<-2
 
 #### FUNZIONI
 # ResBEC   <- BEC(2,"IT","US",2020,2)
