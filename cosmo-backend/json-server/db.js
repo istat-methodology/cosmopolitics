e7c78cdbbd00e3d69e0e566a8f653725d041e87d
papizzo <papizzo@istat.it>
2022-02-02 13:14:10 +0100

    fix json server db.js

--- cosmo-backend/json-server/db.js
+++ cosmo-backend/json-server/db.js
@@ -1,20 +1,19 @@
-const fs=require("fs")
-
+const fs = require("fs");
 //classification
 const flows = JSON.parse(fs.readFileSync("./data/classification/clsFlow.json"));
 const products = JSON.parse(fs.readFileSync("./data/classification/clsProducts.json"));
 const product3s = JSON.parse(fs.readFileSync("./data/classification/clsProducts3.json"));
+const productplus = JSON.parse(fs.readFileSync("./data/classification/clsProductplus.json"));
 const transports = JSON.parse(fs.readFileSync("./data/classification/clsTransport.json"));
 const becs = JSON.parse(fs.readFileSync("./data/classification/clsBec.json"));
 const partners = JSON.parse(fs.readFileSync("./data/classification/clsPartners.json"));
-const productplus = JSON.parse(fs.readFileSync("./data/classification/clsProductplus.json"));
-
 //general json
 //service static
 const countries = JSON.parse(fs.readFileSync("./data/general/countries.json"));
 //service runtime server
+// slider && select period 
+// input for start end date
 const timeperiod = JSON.parse(fs.readFileSync("./data/general/timeperiod.json"));
-
 //map json
 //service static
 const countriesBorders = JSON.parse(fs.readFileSync("./data/map/countriesBorders.json"));
@@ -24,18 +23,9 @@ const ieinfo = JSON.parse(fs.readFileSync("./data/map/ieinfo.json"));
 //marker && features
 const importseries = JSON.parse(fs.readFileSync("./data/map/importseries.json"));
 const exportseries = JSON.parse(fs.readFileSync("./data/map/exportseries.json"));
-
 //trade
 const exportvqs = JSON.parse(fs.readFileSync("./data/trade/exportvqs.json"));
 const importvqs = JSON.parse(fs.readFileSync("./data/trade/importvqs.json"));
-
-
-//
-
-
-// slider 
-// input for start end date
-
 
 module.exports = () => ({
   //map 
@@ -43,8 +33,7 @@ module.exports = () => ({
   //marker
   countries,
   //feature
-  countriesBorders,
-  
+  countriesBorders,  
   //data
   //view box on country  
   ieinfo,
@@ -55,19 +44,15 @@ module.exports = () => ({
   //trade
   //data
   exportvqs,
-  importvqs,
-  //graph
-  
-
+  importvqs,    
   //classification
-  flows,
+  flows,  
   transports,
   products,
   product3s,
-  becs,
-  partners,
   productplus,
-  
+  becs,  
+  partners,  
   // service slider select
   timeperiod
 });
