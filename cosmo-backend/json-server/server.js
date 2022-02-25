efe97df9706c5a47eca7d682c5d24d674501a083
francescoamato <francamato@gmail.com>
2022-02-25 13:47:14 +0100

    add stop

--- cosmo-backend/json-server/server.js
+++ cosmo-backend/json-server/server.js
@@ -5,6 +5,9 @@ var middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
+server.get('/stop',(req,res)=>{
+  process.exit(1);
+});
 server.listen(5300, function () {
   console.log("JSON Server is running");
 });
