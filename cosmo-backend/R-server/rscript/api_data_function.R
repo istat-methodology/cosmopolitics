2a21c74b71087528d1d73d7b820f3739569d45e9
federicobrogi <81154729+federicobrogi@users.noreply.github.com>
2022-02-25 16:11:37 +0100

    Update api_data_function.R

--- cosmo-backend/R-server/rscript/api_data_function.R
+++ cosmo-backend/R-server/rscript/api_data_function.R
@@ -12,14 +12,14 @@ data_function<-function(flow,var_cpa,country_code,partner_code,dataType,tipo_var
                  & PARTNER_ISO==partner_code
                  & cpa==var_cpa)
   
-  
+  #utente seleziona se dati in valore o quantità
   if (tipo_var==1) {
     dati <- dati[,c(5:6)]   #dati in valore
   } else if (tipo_var==2) {
     dati <- dati[,c(5,7)]   #dati in qta
   }
   colnames(dati)<-c("PERIOD","series")
-    
+  
   #metto le date nel formato per l'ordinamento
   dati$year<-substring(dati$PERIOD,1,4)
   dati$month<-str_sub(dati$PERIOD,-2)
@@ -32,13 +32,28 @@ data_function<-function(flow,var_cpa,country_code,partner_code,dataType,tipo_var
   #ordino il dataset
   dati<-dati[order(dati$year,dati$month),]
   
-  #creo le date nel formato per l'output
   date<-paste(dati$year,dati$month,"01",sep="-")
   date<-as.Date(date)
-  
   dati$date <- date
+  
+  
+  #creo le date per il confronto
+  strdate = paste("01",paste(dati$month[1],dati$year[1],sep="/"),sep="/")
+  enddate = paste("01",paste(dati$month[length(dati$month)],dati$year[length(dati$year)],sep="/"),sep="/")
+  
+  date_full = seq.Date(from =as.Date(strdate, "%d/%m/%Y"),
+                  to=as.Date(enddate, "%d/%m/%Y"),by="month")
+  
+  #selezione sono le colonne necessarie
   dati<-dati[,c("date","series")]
   
+  #confronto per mesi missing
+  if (length(date)<length(date_full)) {
+    db_full<-as.data.frame(date_full)
+    colnames(db_full)<-c("date")
+    dati <- dati %>% full_join(db_full)
+  }
+
   #lunghezza db
   l<-length(dati$series)
   
