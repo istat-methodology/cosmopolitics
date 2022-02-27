d694dcc8245ca88a3e09c681ed85215a40c2db93
federicobrogi <81154729+federicobrogi@users.noreply.github.com>
2022-02-27 21:10:03 +0100

    Update api_loadcomext_function.r

--- cosmo-backend/R-server/rscript/api_loadcomext_function.r
+++ cosmo-backend/R-server/rscript/api_loadcomext_function.r
@@ -1,15 +1,16 @@
 load_comext <- function(flow){
   
-  
   if (flow==1) {
-      db <- fread(paste(basedirData,"comext_imp.csv",sep="/"),colClasses = 'character')
+      db <- fread(paste(basedirData,"comext_imp.csv",sep="\\"), 
+                  colClasses=c("cpa"="character"))
 	print("loaded file COMEXT_IMP")
     
   } else if (flow==2) {
-      db <- fread(paste(basedirData,"comext_exp.csv",sep="/"),colClasses = 'character',)
+      db <- fread(paste(basedirData,"comext_exp.csv",sep="/"), 
+                  colClasses=c("cpa"="character"))
 	print("loaded file COMEXT_EXP")
   } 
-  print(head(db))
+  
   return(db)
   
 }
