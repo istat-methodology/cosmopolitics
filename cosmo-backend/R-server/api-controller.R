89b90c79955e8733c96624424b30635631fd2de6
federicobrogi <81154729+federicobrogi@users.noreply.github.com>
2022-02-27 21:08:18 +0100

    Update api-controller.R

--- cosmo-backend/R-server/api-controller.R
+++ cosmo-backend/R-server/api-controller.R
@@ -4,7 +4,7 @@
  library(stringr)
  library(stringi)
  library(dplyr)
-
+ 
 #basedir=("C:\\Users\\ibuku\\git\\hack-backend\\R-server")
 
 #setwd("/home/is2admin/hackathon/git/hack-backend/R-server")
@@ -12,8 +12,8 @@
 #basedir = ("/home/is2admin/hackathon/git/cosmopolitics/cosmo-backend/R-server/rscript")
 #basedirData=("/home/is2admin/hackathon/git/cosmopolitics/cosmo-backend/R-server/data")
 
-#basedir = ("C:\\git\\cosmopolitics\\cosmo-backend\\R-server\\rscript")
-#basedirData=("C:\\git\\cosmopolitics\\cosmo-backend\\R-server\\data")
+# basedir = ("C:\\git\\cosmopolitics\\cosmo-backend\\R-server\\rscript")
+# basedirData=("C:\\git\\cosmopolitics\\cosmo-backend\\R-server\\data")
 
 # basedir = ("d:/development/cosmopolitics/cosmo-backend/R-server/rscript")
 # basedirData=("d:/development/cosmopolitics/cosmo-backend/R-server/data")
@@ -37,11 +37,11 @@ COMEXT_IMP<-load_comext("1")
 COMEXT_EXP<-load_comext("2")
 
 # flow<-1
-# var_cpa<-2
+# var_cpa<-"03"
 # country_code<-"IT"
 # partner_code<-"US"
-# dataType<-1
-# tipo_var<-1 #1val_cpa 2 q_kg
+# dataType<-1 #1 yearly variation, 2 raw data
+# tipo_var<-1 #1==val_cpa; 2==q_kg
 
 #### FUNZIONI
 
@@ -49,7 +49,7 @@ app$add_get(
   path = "/hello", 
   FUN = function(.req, .res) {
      
-    .res$set_body("version 1.0")
+    .res$set_body("world")
     .res$set_content_type("text/plain")
     .res$set_header("Access-Control-Allow-Origin", "*")
     .res$set_header("Access-Control-Allow-Methods","*")
@@ -75,8 +75,8 @@ app$add_get(
 
 
 ### selezione dei dati in base ai filtri utente e visualizzazione serie
-#ITSA  <- itsa(1,3,"IT","US",1,1) 
-# http://localhost:5000/itsa?flow=1&var=3&country=IT&partner=US&dataType=1&tipovar=1
+#ITSA  <- itsa(1,"05","IT","US",1,1) 
+# http://localhost:5000/itsa?flow=1&var=03&country=IT&partner=US&dataType=1&tipovar=1
 app$add_get(
     path = "/itsa", 
       FUN = function(.req, .res) {
@@ -97,5 +97,3 @@ app$add_get(
 
 backend = BackendRserve$new()
 backend$start(app, http_port = 5000)
-
-
