e34dacf39d7bd1a664a688d9e0498858286b54dc
mauroIstat <mbruno@istat.it>
2022-02-27 12:09:46 +0100

    Code refactoring

--- cosmo-frontend/src/App.vue
+++ cosmo-frontend/src/App.vue
@@ -1,27 +1,26 @@
 <template>
   <div id="app">
     <div class="c-app">
-    <app-sidebar></app-sidebar>
-    <CWrapper>
-      <app-toast></app-toast>
-      <app-header />
-      <div class="c-body">
-        <main class="c-main">
-          <CContainer fluid>
-            <transition name="fade" mode="out-in">
-              <router-view></router-view>
-            </transition>
-          </CContainer>
-        </main>
-        <app-footer />
-      </div>
-    </CWrapper>
-  </div>
+      <app-sidebar></app-sidebar>
+      <CWrapper>
+        <app-toast></app-toast>
+        <app-header />
+        <div class="c-body">
+          <main class="c-main">
+            <CContainer fluid>
+              <transition name="fade" mode="out-in">
+                <router-view></router-view>
+              </transition>
+            </CContainer>
+          </main>
+          <app-footer />
+        </div>
+      </CWrapper>
+    </div>
   </div>
 </template>
 
 <script>
-
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Sidebar from "@/components/Sidebar";
@@ -35,25 +34,20 @@ export default {
     "app-toast": Toast
   },
   created() {
-    //Manage browser refresh F5
-    this.$store.dispatch("auth/reloadCredentials");
     //Clear messages
     this.$store.dispatch("message/clear");
     this.$store.dispatch("coreui/clearContext");
     // load metadata
-    this.$store.dispatch("metadata/getMetadata");
-
-    // load classifications
-    
-    this.$store.dispatch("classification/getTransports");
-    this.$store.dispatch("classification/getProducts");
-    this.$store.dispatch("classification/getCountries");
-    this.$store.dispatch("classification/getPartners");
-    
-    this.$store.dispatch("classification/getProductsCPA");
-    this.$store.dispatch("classification/getProductsIntra");
-    this.$store.dispatch("classification/getProductsExtra");
-    
+    this.$store.dispatch("metadata/getMetadata").then(() => {
+      // load classifications
+      this.$store.dispatch("classification/getTransports");
+      this.$store.dispatch("classification/getCountries");
+      this.$store.dispatch("classification/getPartners");
+      this.$store.dispatch("classification/getProducts");
+      this.$store.dispatch("classification/getProductsCPA");
+      this.$store.dispatch("classification/getProductsIntra");
+      this.$store.dispatch("classification/getProductsExtra");
+    });
   }
 };
 </script>
