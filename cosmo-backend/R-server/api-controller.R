972f5665fc22ca75bfbb96c77af558e683c473d0
federicobrogi <81154729+federicobrogi@users.noreply.github.com>
2022-02-28 15:00:46 +0100

    Update api-controller.R

--- cosmo-backend/R-server/api-controller.R
+++ cosmo-backend/R-server/api-controller.R
@@ -1,29 +1,17 @@
- library(RestRserve)
+  library(RestRserve)
  library(jsonlite)
  library(data.table)
  library(stringr)
  library(stringi)
  library(dplyr)
  
-#basedir=("C:\\Users\\ibuku\\git\\hack-backend\\R-server")
-
-#setwd("/home/is2admin/hackathon/git/hack-backend/R-server")
-#setwd("C:\\Users\\ibuku\\git\\hack-backend\\R-server")
-#basedir = ("/home/is2admin/hackathon/git/cosmopolitics/cosmo-backend/R-server/rscript")
-#basedirData=("/home/is2admin/hackathon/git/cosmopolitics/cosmo-backend/R-server/data")
-
+ 
 # basedir = ("C:\\git\\cosmopolitics\\cosmo-backend\\R-server\\rscript")
 # basedirData=("C:\\git\\cosmopolitics\\cosmo-backend\\R-server\\data")
 
-# basedir = ("d:/development/cosmopolitics/cosmo-backend/R-server/rscript")
-# basedirData=("d:/development/cosmopolitics/cosmo-backend/R-server/data")
 
-
-basedir = ("/app/rscript")
-basedirData=("/app/data")
-
-FILE_COMEXT_IMP=paste(basedirData,"comext_imp.csv",sep="/")
-FILE_COMEXT_EXP=paste(basedirData,"comext_exp.csv",sep="/")
+basedir = ("./rscript")
+basedirData=("./data")
 
 source(paste(basedir,"api_loadcomext_function.r",sep="/"))
 source(paste(basedir,"api_data_function.R",sep="/"))
@@ -49,7 +37,7 @@ app$add_get(
   path = "/hello", 
   FUN = function(.req, .res) {
      
-    .res$set_body("version 1.0")
+    .res$set_body("world")
     .res$set_content_type("text/plain")
     .res$set_header("Access-Control-Allow-Origin", "*")
     .res$set_header("Access-Control-Allow-Methods","*")
@@ -75,18 +63,16 @@ app$add_get(
 
 
 ### selezione dei dati in base ai filtri utente e visualizzazione serie
-#ITSA  <- itsa(1,"05","IT","US",1,1) 
+#ITSA  <- itsa(1,"03","IT","US",1,1) 
 # http://localhost:5000/itsa?flow=1&var=03&country=IT&partner=US&dataType=1&tipovar=1
 app$add_get(
     path = "/itsa", 
       FUN = function(.req, .res) {
-        print("/itsa")
         resp<-itsa(.req$get_param_query("flow"),.req$get_param_query("var"),
                  .req$get_param_query("country"),.req$get_param_query("partner"),
                  .req$get_param_query("dataType"),.req$get_param_query("tipovar"))
     .res$set_body(resp)
-    print("/itsa ok")
-
+  
     .res$set_header("Access-Control-Allow-Origin", "*")
     .res$set_header("Access-Control-Allow-Methods","*")
     .res$set_header("Access-Control-Allow-Headers", "*")
