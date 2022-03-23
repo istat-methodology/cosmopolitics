09a1d72f0f3c1655a92efb3bd8a89f4d2925cfae
mauroIstat <mbruno@istat.it>
2022-03-23 14:28:27 +0100

    Added drag drop panel (styles ugly)

--- cosmo-frontend/src/views/graph/GraphScenario.vue
+++ cosmo-frontend/src/views/graph/GraphScenario.vue
@@ -5,20 +5,52 @@
     :closeOnBackdrop="false"
     @update:show="closeModal"
   >
-    <CDataTable :items="items" hover sorter />
-    <div class="row">
-      <div class="col-5">
-        <ul>
-          <li v-for="transport in transports" :key="transport.id">
-            {{ transport.descr }}
-          </li>
-        </ul>
+    <CDataTable
+      v-if="data"
+      :items="data"
+      :fields="fields"
+      column-filter
+      :items-per-page="5"
+      sorter
+      hover
+      pagination
+    />
+    <!-- Drag'n drop -->
+    <div class="row drag-container" v-if="displayTransport">
+      <div
+        class="col-5 drop-zone"
+        @drop="onDropTransports($event)"
+        @dragenter.prevent
+        @dragover.prevent
+      >
+        <div
+          v-for="transport in transports"
+          :key="transport.id"
+          class="drag-el"
+          draggable="true"
+          @dragstart="startDrag($event, transport)"
+        >
+          {{ transport.descr }}
+        </div>
       </div>
       <div class="col-2">
         <!-- Nothing -->
       </div>
-      <div class="col-5">
-        <!-- Scenario-->
+      <div
+        class="col-5 drop-zone"
+        @drop="onDropScenario($event)"
+        @dragenter.prevent
+        @dragover.prevent
+      >
+        <div
+          v-for="scenarioTransport in scenarioTransports"
+          :key="scenarioTransport.id"
+          class="drag-el"
+          draggable="true"
+          @dragstart="startDrag($event, scenarioTransport)"
+        >
+          {{ scenarioTransport.descr }}
+        </div>
       </div>
     </div>
     <template #footer>
@@ -38,24 +70,70 @@
 <script>
 export default {
   name: "GraphScenario",
-  data: () => ({
-    scenarioTransports: []
-  }),
   props: {
     showModal: {
       type: Boolean,
       default: false
     },
-    items: {
+    displayTransport: {
+      type: Boolean,
+      default: false
+    },
+    data: {
       type: Array,
       default: () => null
     },
-    transports: {
+    fields: {
+      type: Array,
+      default: () => []
+    },
+    selectedTransports: {
+      type: Array,
+      default: () => null
+    },
+    selectedScenarioTransports: {
       type: Array,
       default: () => null
     }
   },
+  computed: {
+    transports: {
+      get() {
+        return this.selectedTransports;
+      },
+      set(value) {
+        this.$emit("updateTransports", value);
+      }
+    },
+    scenarioTransports: {
+      get() {
+        return this.selectedScenarioTransports;
+      },
+      set(value) {
+        this.$emit("updateScenarioTransports", value);
+      }
+    }
+  },
   methods: {
+    startDrag(event, item) {
+      event.dataTransfer.dropEffect = "move";
+      event.dataTransfer.effectAllowed = "move";
+      event.dataTransfer.setData("itemID", item.id);
+    },
+    onDropTransports(event) {
+      const itemId = event.dataTransfer.getData("ItemID");
+      const transport = this.scenarioTransports.find(tr => tr.id == itemId);
+      this.scenarioTransports = this.scenarioTransports.filter(
+        tr => tr.id != itemId
+      );
+      this.transports.push(transport);
+    },
+    onDropScenario(event) {
+      const itemId = event.dataTransfer.getData("ItemID");
+      const transport = this.transports.find(tr => tr.id == itemId);
+      this.transports = this.transports.filter(tr => tr.id != itemId);
+      this.scenarioTransports.push(transport);
+    },
     closeModal() {
       this.$emit("closeModal");
     },
@@ -65,3 +143,23 @@ export default {
   }
 };
 </script>
+<style scoped>
+.drag-container {
+  margin-right: 0;
+  margin-left: 0;
+}
+.drop-zone {
+  background-color: #ebedef;
+  padding: 10px;
+  min-height: 50px;
+}
+.drag-el {
+  background-color: #321fdb;
+  color: white;
+  padding: 5px;
+  margin-bottom: 10px;
+}
+.drag-el:nth-last-of-type(1) {
+  margin-bottom: 0;
+}
+</style>
