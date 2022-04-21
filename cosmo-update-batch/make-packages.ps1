85d2e53129745094dcdbbe8c6fee93325e08ad92
Luca Giuliani <giulianil@microsoft.com>
2022-04-21 19:54:42 +0200

    * Increased verbosity in prepare-node.sh. * Added Powershell script make-packages.ps1 to create .zip Azure Batch Application Package(s).

---
+++ cosmo-update-batch/make-packages.ps1
@@ -0,0 +1,7 @@
+$Version = ((Get-Content .\prepare-node.sh | ?{ $_ -match '^# Version [0-9\.]+$' }) -split ' ')[2]
+Compress-Archive -Update -Path .\prepare-node.sh -DestinationPath .\prepare-node_$Version.zip
+
+$Version = ((Get-Content .\cosmoDataUpdate.py | ?{ $_ -match '^# Version [0-9\.]+$' }) -split ' ')[2]
+Compress-Archive -Update -Path .\cosmoDataUpdate.py, .\run.sh, .\requirements.txt -DestinationPath .\prepare-node_$Version.zip
+
+Write-Host "Operation completed."
