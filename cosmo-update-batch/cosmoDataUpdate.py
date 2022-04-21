0a18b1e2c996fbadd8aa318ad7e759c9b133098d
Luca Giuliani <giulianil@microsoft.com>
2022-04-21 19:57:25 +0200

    * Fixed name in make-packages.ps1.

--- cosmo-update-batch/cosmoDataUpdate.py
+++ cosmo-update-batch/cosmoDataUpdate.py
@@ -1,4 +1,4 @@
-# version 0.2.0
+# Version 0.3.0
 
 import os
 import sys
--- cosmo-update-batch/make-packages.ps1
+++ cosmo-update-batch/make-packages.ps1
@@ -2,6 +2,6 @@ $Version = ((Get-Content .\prepare-node.sh | ?{ $_ -match '^# Version [0-9\.]+$'
 Compress-Archive -Update -Path .\prepare-node.sh -DestinationPath .\prepare-node_$Version.zip
 
 $Version = ((Get-Content .\cosmoDataUpdate.py | ?{ $_ -match '^# Version [0-9\.]+$' }) -split ' ')[2]
-Compress-Archive -Update -Path .\cosmoDataUpdate.py, .\run.sh, .\requirements.txt -DestinationPath .\prepare-node_$Version.zip
+Compress-Archive -Update -Path .\cosmoDataUpdate.py, .\run.sh, .\requirements.txt -DestinationPath .\cosmoDataUpdate_$Version.zip
 
 Write-Host "Operation completed."
