57368dd762b4f74673648741bc57e4fdbaa18232
Luca Giuliani <giulianil@microsoft.com>
2022-06-01 16:53:34 +0200

    * Added customizability of cloud_roleName in AppInsights of the Python Server

--- cosmo-backend/python-server/app.py
+++ cosmo-backend/python-server/app.py
@@ -28,6 +28,10 @@ criterio="VALUE_IN_EUROS" #VALUE_IN_EUROS 	QUANTITY_IN_KG
 def is_application_insight_configured():
     return os.getenv('APPINSIGHTS_INSTRUMENTATIONKEY')!=None or os.getenv('APPLICATIONINSIGHTS_CONNECTION_STRING')!=None
 
+def ai_callback_function(envelope):
+    if os.getenv('CLOUD_ROLE') != None:
+        envelope.tags['ai.cloud.role'] = os.getenv('CLOUD_ROLE')
+
 logging.basicConfig(level=logging.INFO,
     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S')
@@ -35,7 +39,9 @@ logging.basicConfig(level=logging.INFO,
 logger = logging.getLogger(__name__)
 
 if is_application_insight_configured():
-    logger.addHandler(AzureLogHandler())
+    log_handler = AzureLogHandler()
+    log_handler.add_telemetry_processor(ai_callback_function)
+    logger.addHandler(log_handler)
 else:
     logger.warning('Application insights is not configured.')
 
@@ -324,10 +330,13 @@ from opencensus.trace.samplers import ProbabilitySampler
 app = Flask(__name__)
 CORS(app, resources=r'/*')
 
+azure_exporter = AzureExporter()
+azure_exporter.add_telemetry_processor(ai_callback_function)
+
 if is_application_insight_configured():
     middleware = FlaskMiddleware(
         app,
-        exporter=AzureExporter(),
+        exporter=azure_exporter,
         sampler=ProbabilitySampler(rate=1.0),
     )
 
