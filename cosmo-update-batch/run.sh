c03f8a0b2bedc1fa6ae0a0aaf4fde963adf1f4af
Luca Giuliani <giulianil@microsoft.com>
2022-06-01 09:55:33 +0200

    * Added check on disk space before running the python script.

--- cosmo-update-batch/run.sh
+++ cosmo-update-batch/run.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+MIN_WORKING_FOLDER_SPACE_GB=100
+
 pip install -r requirements.txt
+if [ -z "$WORKING_FOLDER" ]; then
+   echo "WORKING_FOLDER not specified. Aborting." 1>&2
+   exit 1
+fi
+FREESPACE_ON_WORKING_FOLDER=$(( $(stat -f --format="%a" $WORKING_FOLDER) * $(stat -f --format="%S" $WORKING_FOLDER) / 1024 / 1024 / 1024 ))
+echo "Free Space on $WORKING_FOLDER: $FREESPACE_ON_WORKING_FOLDER GB"
+if [ $FREESPACE_ON_WORKING_FOLDER -lt $MIN_WORKING_FOLDER_SPACE_GB ]; then
+   echo "Insufficient disk space. Aborting." 1>&2
+   exit 1
+fi
 python cosmoDataUpdate.py
