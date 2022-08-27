3647dd196ccb863d46923f93e516bc3b9b8b6209
mauroIstat <mbruno@istat.it>
2022-08-27 16:59:04 +0200

    Added prettier endofline rule

--- cosmo-frontend/.eslintrc.js
+++ cosmo-frontend/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/multi-word-component-names": "off",
-    "vue/no-reserved-component-names": "off"
+    "vue/no-reserved-component-names": "off",
+    "prettier/prettier": ["error", { endOfLine: "auto" }]
   }
 }
